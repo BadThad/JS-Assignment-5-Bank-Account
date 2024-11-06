e87b824a4fa8e75be12074045999fb634122c21b
Peter Fänge <pellefange@gmail.com>
2024-11-06 19:07:42 +0100

    finished all functions

--- index.js
+++ index.js
@@ -2,35 +2,64 @@ const account = {
     accountName: "John",
     balance: 1000,
     
-    getAccountName() {
-        console.log(`Hello ${this.accountName}, welcome to your account.`)
+    getAccountName: function() {
+        alert(`Hello ${this.accountName}, welcome to your account.`);
+        atm();
     },
     
-    getBalance() {
-        console.log(`Your current balance is $${this.balance}`);
+    getBalance: function() {
+        alert(`Your current balance is $${this.balance}`);
+        atm();
     },
 
-    deposit() {
-
-    },
-
-    withdraw() {
-
-    },
-
-    accountError() {
-        if () {
-
+    deposit: function() {
+        const deposit = parseFloat(prompt("How much would you like to deposit?"));
+        if (isNaN(deposit) || deposit === '' || deposit < 0) {
+                alert(`Error: please enter a valid amount.`);
+                deposit();
         } else {
-
+            balance += deposit;
+            this.getBalance();
         }
     },
 
-    exitAccount() {
-        console.log(`Bye ${this.accountName}, see you again soon!`)
-        return
+    withdraw: function() {
+        const withdraw = parseFloat(prompt("How much would you like to withdraw?"));
+        if (isNaN(withdraw) || withdraw === '' || withdraw < 0) {
+                alert(`Error: please enter a valid amount.`);
+                withdraw();
+        } else {
+            balance -= withdraw;
+            this.getBalance();
+        }
     },
-}
 
-account.getAccountName()
-account.getBalance()
+    error: function() {
+        alert("Error: please pick a number between 1 - 5.");
+    },
+
+    exitAccount: function() {
+        const confirm_exit = confirm("Exiting program.");
+        if (confirm_exit) {
+            window.close();
+        } else {
+            atm();
+        }
+    },
+
+    atm: function() {
+        const message = parseFloat (
+            prompt(
+                "Select a number 1.) Get account name, 2.) See balance, 3.) Make a deposit, 4.) Make a withdrawal, 5.) Exit"
+            )
+        );
+    
+        switch (message) {
+            case 1: getAccountName();
+            case 2: getBalance();
+            case 3: deposit();
+            case 4: withdraw();
+            case 5: exit();
+        }
+    }
+}
